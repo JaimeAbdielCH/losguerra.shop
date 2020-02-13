a0181f1a7c6aefc1eecd80824a963a4683319946
Jaime Abdiel Chung <jaime.abdiel@hotmail.com>
2020-02-13 00:32:24 -0500

    tarifa plana de envio

--- catalog/model/extension/shipping/flat.php
+++ catalog/model/extension/shipping/flat.php
@@ -3,8 +3,9 @@ class ModelExtensionShippingFlat extends Model {
 	function getQuote($address) {
 		$this->load->language('extension/shipping/flat');
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('shipping_flat_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
-
+		$querystr = "SELECT *, (SELECT REPLACE(description, '$ ', '') FROM oc_geo_zone WHERE geo_zone_id = a.geo_zone_id) valor FROM " . DB_PREFIX . "zone_to_geo_zone a WHERE geo_zone_id = '" . (int)$address['geo_zone_id']  . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')";
+		
+		$query = $this->db->query($querystr);
 		if (!$this->config->get('shipping_flat_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
@@ -21,9 +22,9 @@ class ModelExtensionShippingFlat extends Model {
 			$quote_data['flat'] = array(
 				'code'         => 'flat.flat',
 				'title'        => $this->language->get('text_description'),
-				'cost'         => $this->config->get('shipping_flat_cost'),
+				'cost'         => $query->row['valor'],
 				'tax_class_id' => $this->config->get('shipping_flat_tax_class_id'),
-				'text'         => $this->currency->format($this->tax->calculate($this->config->get('shipping_flat_cost'), $this->config->get('shipping_flat_tax_class_id'), $this->config->get('config_tax')), $this->session->data['currency'])
+				'text'         => $this->currency->format($this->tax->calculate($query->row['valor'], $this->config->get('shipping_flat_tax_class_id'), $this->config->get('config_tax')), $this->session->data['currency'])
 			);
 
 			$method_data = array(
