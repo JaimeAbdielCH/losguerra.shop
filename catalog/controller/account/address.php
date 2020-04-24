9a42dd5c851ff52ca254ea5722eea0e2c20fce3a
Jaime Abdiel Chung <jaime.abdiel@hotmail.com>
2020-04-24 15:27:13 -0500

    FIXING ONE BUG ADDING ADDRESS MANUALLY

--- catalog/controller/account/address.php
+++ catalog/controller/account/address.php
@@ -444,7 +444,7 @@ class ControllerAccountAddress extends Controller {
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
-
+		$data['googleapikey'] = GOOGLEMAPAPIKEY;
 		$this->response->setOutput($this->load->view('account/address_form', $data));
 	}
 
