bad9c4a88e57705018fef9ab7e7318f85d0351f1
root <root@localhost.jannahOne>
2023-02-01 15:15:42 -0500

    finish security warnings

---
+++ lgshopapp/gerencia/config.php
@@ -0,0 +1,36 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+define('HTTP_SERVER', 'http://losguerra.shop/gerencia/');
+define('HTTP_CATALOG', 'http://losguerra.shop/');
+
+// DIR
+define('DIR_OPENCART', '/lgshopapp/');
+define('DIR_APPLICATION', DIR_OPENCART . 'gerencia/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_STORAGE', '//storageout/store/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'pdo');
+define('DB_HOSTNAME', 'db');
+define('DB_USERNAME', 'losguerradbone');
+define('DB_PASSWORD', 'userdb2023');
+define('DB_DATABASE', 'losguerradbone');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
