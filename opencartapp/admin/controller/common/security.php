8615bf70dfffbbc1af3838368587bdcae17f01f1
Jaime Chung <Jaime Chung@DESKTOP-II5ACUL>
2023-02-28 17:15:33 -0500

    fixing security replace string

--- opencartapp/admin/controller/common/security.php
+++ opencartapp/admin/controller/common/security.php
@@ -118,7 +118,7 @@ class Security extends \Opencart\System\Engine\Controller {
 		}
 
 		if (isset($this->request->get['name'])) {
-			$name = preg_replace('[^a-zA-z0-9_]', '', basename(html_entity_decode(trim($this->request->get['name']), ENT_QUOTES, 'UTF-8')));
+			$name = $this->request->get['name']; //$name = preg_replace('[^a-zA-z0-9_]', '', basename(html_entity_decode(trim($this->request->get['name']), ENT_QUOTES, 'UTF-8')));
 		} else {
 			$name = '';
 		}
