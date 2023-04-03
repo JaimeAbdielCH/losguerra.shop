c69309a7c1e2f7bade6e7cee8d935a873c6a3427
Jaime Chung <Jaime Chung@DESKTOP-II5ACUL>
2023-04-03 14:01:21 -0500

    flat rate automatico

--- opencartapp/extension/opencart/catalog/model/shipping/flat.php
+++ opencartapp/extension/opencart/catalog/model/shipping/flat.php
@@ -3,8 +3,9 @@ namespace Opencart\Catalog\Model\Extension\Opencart\Shipping;
 class Flat extends \Opencart\System\Engine\Model {
 	function getQuote(array $address): array {
 		$this->load->language('extension/opencart/shipping/flat');
-
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('shipping_flat_geo_zone_id') . "' AND `country_id` = '" . (int)$address['country_id'] . "' AND (`zone_id` = '" . (int)$address['zone_id'] . "' OR `zone_id` = '0')");
+		//`geo_zone_id` = '" . (int)$this->config->get('shipping_flat_geo_zone_id') . "' AND 
+		$querystr = "SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('shipping_flat_geo_zone_id') . "' AND `country_id` = '" . (int)$address['country_id'] . "' AND (`zone_id` = '" . (int)$address['zone_id'] . "' OR `zone_id` = '0')";
+		$query = $this->db->query($querystr);
 
 		if (!$this->config->get('shipping_flat_geo_zone_id')) {
 			$status = true;
@@ -18,11 +19,10 @@ class Flat extends \Opencart\System\Engine\Model {
 
 		if ($status) {
 			$quote_data = [];
-
 			$quote_data['flat'] = [
 				'code'         => 'flat.flat',
 				'title'        => $this->language->get('text_description'),
-				'cost'         => $this->config->get('shipping_flat_cost'),
+				'cost'         => $this->tax->calculate((float)$this->config->get('shipping_flat_cost'), $this->config->get('shipping_flat_tax_class_id')),
 				'tax_class_id' => $this->config->get('shipping_flat_tax_class_id'),
 				'text'         => $this->currency->format($this->tax->calculate($this->config->get('shipping_flat_cost'), $this->config->get('shipping_flat_tax_class_id'), $this->config->get('config_tax')), $this->session->data['currency'])
 			];
